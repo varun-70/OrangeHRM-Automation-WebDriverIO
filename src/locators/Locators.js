b212388dcd884c73bdea963db26efe7c9e1704a0
Varun M S <testvagrant@TestVagrants-MacBook-Pro-3.local>
2025-01-03 10:33:58 +0530

    Update Locators with mobile elements

--- src/locators/Locators.js
+++ src/locators/Locators.js
@@ -7,19 +7,19 @@
 const LoginPageLocators = {
     fields: {
         username: {
-            android: '', // TODO: 
+            android: '~Username input field',
             ios: '', // TODO: 
             web: 'input[name="username"]',
         },
         password: {
-            android: '',
+            android: '~Password input field',
             ios: '',
             web: 'input[name="password"]',
         },
     },
     buttons: {
         login: {
-            android: '',
+            android: '~Login button',
             ios: '',
             web: 'button[type="submit"]',
         },
@@ -27,24 +27,48 @@ const LoginPageLocators = {
 };
 
 /**
- * Locators for the Login page elements.
- * @typedef {Object} LoginPageLocators
+ * Locators for the Home page elements.
+ * @typedef {Object} HomePageLocators
  * @property {Object} fields - Input field locators
  * @property {Object} buttons - Button locators
  */
 const HomePageLocators = {
     buttons: {
         views: {
-            android: 'Views',
+            android: '~Views',
             ios: '',
             web: '',
         },
         animation: {
-            android: 'Animation',
+            android: '~Animation',
             ios: '',
             web: '',
-        }
-    }
-}
+        },
+        sideMenu: {
+            android: '~open menu',
+            ios: '',
+            web: '',
+        },
+    },
+};
 
-export default { LoginPageLocators, HomePageLocators };
+/**
+ * Locators for the Side menu elements.
+ * @typedef {Object} SideMenuLocators
+ * @property {Object} fields - Input field locators
+ * @property {Object} buttons - Button locators
+ */
+const SideMenuLocators = {
+    buttons: {
+        logIn: {
+            android: '~menu item log in',
+            ios: '',
+            web: '',
+        },
+    },
+};
+
+export default { LoginPageLocators, HomePageLocators, SideMenuLocators };
+// export default LoginPageLocators;
+// export default HomePageLocators;
+// export default SideMenuLocators;
